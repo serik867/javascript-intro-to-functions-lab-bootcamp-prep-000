259771618fe2a7d97342f62c4ea59d91abfef3a4
serik867 <durbayevserdar@hotmail.com>
2017-10-12 01:47:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+ function sayHiToGrandma(string){
+   if(string.toLowerCase){
+     console.log("I can't hear you!")
+   }
+   
+ }
